1c059d651ca32a73b948b5aa7f3f01c75e6a3d6f
Travis A. Everett <travis.a.everett@gmail.com>
2013-08-08 16:56:21 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='soe_api',
+    version='0.1.0',
+    author='Travis A. Everett',
+    author_email='travis.a.everett@gmail.comm',
+    packages=['soe', 'soe.Stats', 'ps2', 'ps2.Stats', 'eq2', 'eq2.Stats'],
+    url='https://github.com/abathur/soe_api',
+    license='LICENSE.md',
+    description='Rudimentary Python API package for Sony Online Entertainment (SOE)'s stats/census API, includes Planetside 2 and Everquest 2.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "requests",
+    ],
+)
